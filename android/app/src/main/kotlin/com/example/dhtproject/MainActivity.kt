4008276f6d992c72a013d20282c63bb6414418f8
AstroBesat <128177174+AstroBesat-SoftW@users.noreply.github.com>
2024-02-21 12:43:57 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/dhtproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.dhtproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/example/erenbalcidernegi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.erenbalcidernegi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
